c03532f7f4237a6be5cb87fb6c018c43dec8fce6
Shaunak Joshi <shaunakjoshi1407@gmail.com>
2026-04-23 20:20:10 -0500

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,20 @@
+class MinStack:
+
+    def __init__(self):
+        self.min_stack = []
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        curr_min = self.min_stack[-1] if self.min_stack else val
+        self.min_stack.append(min(curr_min, val))
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
