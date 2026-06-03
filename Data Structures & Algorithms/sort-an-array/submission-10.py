d67c1bbecc2938641b0139cabce1807f90dac726
Shaunak Joshi <shaunakjoshi1407@gmail.com>
2026-06-02 19:17:49 -0500

    Add: sort-an-array - submission-10

---
+++ Data Structures & Algorithms/sort-an-array/submission-10.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def counting_sort():
+            freq = {}
+
+            for num in nums:
+                freq[num] = 1 + freq.get(num, 0)
+
+            index = 0
+            min_val, max_val = min(nums), max(nums)
+            for i in range(min_val, max_val + 1):
+                while freq.get(i, 0) > 0:
+                    nums[index] = i
+                    index += 1
+                    freq[i] -= 1
+
+        counting_sort()
+        return nums
