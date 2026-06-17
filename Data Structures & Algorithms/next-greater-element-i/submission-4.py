fd4d7e3f8465f1b8e350e449fd94ca8ca2ed6d97
Shaunak Joshi <shaunakjoshi1407@gmail.com>
2026-06-16 20:53:58 -0500

    Add: next-greater-element-i - submission-4

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
+        freq = {}
+
+        for i, num in enumerate(nums1):
+            freq[num] = i
+        
+        res = [-1] * len(nums1)
+
+        for i in range(len(nums2)):
+            cur = nums2[i]
+
+            while stack and stack[-1] < cur:
+                val = stack.pop()
+                idx = freq[val]
+                res[idx] = cur
+            
+            if cur in freq:
+                stack.append(cur)
+            
+        
+        return res
