2b4b64658aeb0046aef26576dee4c272a7867fb5
Shaunak Joshi <shaunakjoshi1407@gmail.com>
2026-08-16 21:36:23 -0500

    Add: split-array-largest-sum - submission-3

---
+++ Data Structures & Algorithms/split-array-largest-sum/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        def feasible(capacity):
+            total = 0
+            subarray = 1
+
+            for num in nums:
+                total += num
+                if total > capacity:
+                    subarray += 1
+                    if subarray > k:
+                        return False
+                    total = num
+            
+            return True
+
+        l, r = max(nums), sum(nums)
+
+        while l < r:
+            mid = l + (r - l) // 2
+            if feasible(mid):
+                r = mid
+            else:
+                l = mid + 1
+        
+        return l
